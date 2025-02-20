6ca48aa42db9c429c4aa1abdce83ebb902c11b72
SHELLY1209 <aaditpatil1209@gmail.com>
2025-02-20 22:46:37 +0530

    homepage

--- app/src/main/java/com/example/mainproject/MainActivity.java
+++ app/src/main/java/com/example/mainproject/MainActivity.java
@@ -8,17 +8,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView img1 = findViewById(R.id.img_1);
+
+        // Set an OnClickListener to the ImageView
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start another activity
+                Intent intent = new Intent(MainActivity.this, bitcoin.class);
+                startActivity(intent);  // Start the new activity
+            }
         });
     }
 }
