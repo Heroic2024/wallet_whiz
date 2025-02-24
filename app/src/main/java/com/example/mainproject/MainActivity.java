22f944411f492bbeed181cf6e5e91d051815cda1
SHELLY1209 <aaditpatil1209@gmail.com>
2025-02-24 20:44:57 +0530

    run code

--- app/src/main/java/com/example/mainproject/MainActivity.java
+++ app/src/main/java/com/example/mainproject/MainActivity.java
@@ -11,12 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton img1,img3,img4;
+    ImageButton img1,img4;
+    Button img3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
 
         img1 = findViewById(R.id.img_1);
         img3 = findViewById(R.id.img_3);
+        img4 = findViewById(R.id.img_4);
 
 
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start another activity
-                Intent intent1 = new Intent(MainActivity.this, bitcoin.class);
+                Intent intent1 = new Intent(MainActivity.this, profile.class);
                 startActivity(intent1);  // Start the new activity
 
             }
@@ -40,8 +43,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start another activity
-                Intent intent2 = new Intent(MainActivity.this, expense_history.class);
+                Intent intent2 = new Intent(MainActivity.this, expense_tracker.class);
                 startActivity(intent2);  // Start the new activity
+
+            }
+        });
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start another activity
+                Intent intent3 = new Intent(MainActivity.this, expense_history.class);
+                startActivity(intent3);  // Start the new activity
 
             }
         });
